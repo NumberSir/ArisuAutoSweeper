0068df094eb6e5fed52440b10d11f32770ba8fcf
Number_Sir <number_sir@126.com>
2023-11-27 22:09:11 +0800

    refactor(popup): optimize
    - 简化嵌套 if
    - 保护性条件提前
    - 简化返回条件

--- tasks/base/popup.py
+++ tasks/base/popup.py
@@ -13,19 +13,20 @@ class PopupHandler(ModuleBase):
         Returns:
             If handled.
         """
-        if self.appear(LOADING_CHECK):
-            timer = Timer(0.5).start()
-            while 1:
-                if timer.reached_and_reset():
-                    self.device.screenshot()
-                    if self.appear(LOADING_CHECK):
-                        self.device.stuck_record_clear()
-                        continue
-                    else:
-                        break
-            return True
-
-        return False
+        if not self.appear(LOADING_CHECK):
+            return False
+        
+        timer = Timer(0.5).start()
+        while 1:
+            if not timer.reached_and_reset():
+                continue
+                
+            self.device.screenshot()
+            if not self.appear(LOADING_CHECK):
+                break
+                
+            self.device.stuck_record_clear()
+        return True
 
     def handle_reward(self) -> bool:
         """
@@ -35,73 +36,56 @@ class PopupHandler(ModuleBase):
         Returns:
             If handled.
         """
-        if self.appear(GET_REWARD) or self.match_color(GET_REWARD, threshold=30):
-            while 1:
-                self.device.screenshot()
-                if self.appear(GET_REWARD) or self.match_color(GET_REWARD, threshold=30):
-                    self.click_with_interval(GET_REWARD, interval=0.5)
-                else:
-                    break
-            return True
+        if not any([
+            self.appear(GET_REWARD),
+            self.match_color(GET_REWARD, threshold=30)
+        ]):
+            return False
 
-        return False
+        while 1:
+            self.device.screenshot()
+            if not any([
+                self.appear(GET_REWARD),
+                self.match_color(GET_REWARD, threshold=30)
+            ]):
+                break
+            self.click_with_interval(GET_REWARD, interval=0.5)
+        return True
 
     def handle_reward_skip(self, interval=5) -> bool:
         if self.appear_then_click(GET_REWARD_SKIP, interval=interval):
             return True
 
     def handle_daily_news(self, interval=2) -> bool:
-        if self.appear_then_click(DAILY_NEWS, interval=interval):
-            return True
-
-        return False
+        return self.appear_then_click(DAILY_NEWS, interval=interval)
 
     def handle_daily_reward(self, interval=2) -> bool:
-        if self.appear_then_click(DAILY_REWARD, interval=interval):
-            return True
-
-        return False
+        return self.appear_then_click(DAILY_REWARD, interval=interval)
 
     def handle_network_reconnect(self, interval=5) -> bool:
-        if self.appear_then_click(NETWORK_RECONNECT, interval=interval):
-            return True
-        if self.appear_then_click(NETWORK_RECONNECT_OK, interval=interval):
-            return True
-
-        return False
+        return any([
+            self.appear_then_click(NETWORK_RECONNECT, interval=interval),
+            self.appear_then_click(NETWORK_RECONNECT_OK, interval=interval)
+        ])
 
     def handle_affection_level_up(self) -> bool:
-        if self.appear_then_click(AFFECTION_LEVEL_UP):
-            while 1:
-                self.device.screenshot()
-                if self.appear(AFFECTION_LEVEL_UP):
-                    self.click_with_interval(AFFECTION_LEVEL_UP, interval=1)
-                else:
-                    break
-            return True
-
-        return False
+        if not self.appear_then_click(AFFECTION_LEVEL_UP):
+            return False
+        while 1:
+            self.device.screenshot()
+            if not self.appear(AFFECTION_LEVEL_UP):
+                break
+            self.click_with_interval(AFFECTION_LEVEL_UP, interval=1)
+        return True
 
     def handle_new_student(self, interval=5) -> bool:
-        if self.appear_then_click(GET_NEW_STUDENT, interval=interval):
-            return True
-
-        return False
+        return bool(self.appear_then_click(GET_NEW_STUDENT, interval=interval))
 
     def handle_ap_exceed(self, interval=5) -> bool:
-        if self.appear_then_click(AP_EXCEED, interval=interval):
-            return True
-
-        return False
+        return bool(self.appear_then_click(AP_EXCEED, interval=interval))
 
     def handle_insufficient_inventory(self, interval=5) -> bool:
-        if self.appear_then_click(INSUFFICIENT_INVENTORY, interval=interval):
-            return True
-
-        return False
+        return bool(self.appear_then_click(INSUFFICIENT_INVENTORY, interval=interval))
 
     def handle_item_expired(self, interval=5) -> bool:
-        if self.appear_then_click(ITEM_EXPIRED, interval=interval):
-            return True
-
-        return False
+        return bool(self.appear_then_click(ITEM_EXPIRED, interval=interval))
